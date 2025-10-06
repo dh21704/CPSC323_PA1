b22380eae259bbbf8310b04458106afbc4b031b7
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2025-10-05 22:56:18 -0700

    Update pa1.py

--- pa1.py
+++ pa1.py
@@ -1,12 +1,22 @@
 import re #allows to work with regular expression
 
 #+ is one or more occurences
+KEYWORDS = {
+    "int", "float", "double", "char", "void",
+    "if", "else", "while", "for", "return",
+    "break", "continue", "const", "static",
+    "struct", "class", "bool", "true", "false",
+    "using", "namespace", "include"
+}
 
 token_specification = [
     ('IDENTIFIER', r'[A-Za-z_][A-Za-z0-9]*'), #
     ('INTEGER', r'\d+'), #any integers whole numbers, \d is digits
     ('WHITESPACE', r'\s+'), #catches all whitespace's, newlines, and tabs
     ('UNKNOWN', r'.'), #anything that doesnt match the other patterns
+    ('STRING', r'"(\\.|[^"\\])*"'),
+    ('PUNCTUATION',   r'[{}\(\)\[\];,\.]'),   # punctuation    
+    ('OPERATOR',      r'==|!=|<=|>=|\+=|-=|\*=|/=|&&|\|\||\+\+|--|[+\-*/=<>%&^|~!]'),  # operators (multi-char first)
     ]
 
 def lexer(source_code):
@@ -27,10 +37,13 @@ def lexer(source_code):
               #print(f"Found '{i.group()}' at position {i.start()}: {i.end()}")
               for name, value in i.groupdict().items():
                 if value and name != "WHITESPACE":
+                  if name == "IDENTIFIER" and value in KEYWORDS:
+                    name = "KEYWORD"
+                  
                   #print(f"{name:<12} {value}")
                   tokens.append((name, value)) #appending as a tuple
                 
-                  
+                
             
             #print(tokens)  
 
@@ -46,7 +59,7 @@ def lexer(source_code):
 
 t = lexer("input_sourcecode.txt")
 
-with open('output_file.txt', 'w') as file:
+with open('Output.txt', 'w') as file:
   for token_type, lexeme in t:
       line = f"{token_type:<12} {lexeme}\n"
       file.write(line)
